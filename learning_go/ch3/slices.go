ace4e88d7d88c40b9cd1438594eb8e0e47c6e253
James <53650971+jmstudyacc@users.noreply.github.com>
2021-07-21 15:09:07 +0100

    adding verbose output for slice examples

--- learning_go/ch3/slices.go
+++ learning_go/ch3/slices.go
@@ -6,17 +6,17 @@ import "fmt"
 
 func main() {
 	var x = []int{10, 20, 30} // using [...] makes an array [] makes a slice
-	fmt.Println(x)
+	fmt.Printf("\nThis prints out a slice with 3 values:\n%d\n", x)
 
 	var y = []int{1, 5: 4, 6, 10: 100, 15}
-	fmt.Println(y)
+	fmt.Printf("\nThis prints out a slice that places '0' in undeclared positions:\n%d\n", y)
 
 	var z [][]int
-	fmt.Println(z)
+	fmt.Printf("\nThis prints out an empty slice with no values:\n%d\n", z)
 
-	fmt.Println(y[5])
+	fmt.Printf("\nThis prints out the value held in var y at index 5:\n%d\n", y[5])
 
 	// as you can see this is very similar to using arrays, but differences abound!
 	// for instance Slices are not comparable and will produce a compile time error
-	fmt.Println(x == nil) // slices can ONLY be compared against nil
+	fmt.Printf("\nThis prints out the result of the statement, 'x == nil':\n%t\n", x == nil) // slices can ONLY be compared against nil
 }
